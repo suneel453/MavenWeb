bf55e9377576d298c1a8bc3c828a9d95ce0fd888
Malathi <malathim993@gmail.com>
2016-02-05 18:39:31 +0530

    Update HelloWorldServlet.java

--- src/main/java/net/viralpatel/maven/HelloWorldServlet.java
+++ src/main/java/net/viralpatel/maven/HelloWorldServlet.java
@@ -19,7 +19,7 @@ public class HelloWorldServlet extends HttpServlet {
 		resp.setContentType("text/html");
 		
 		PrintWriter out = resp.getWriter();
-		out.print("Hello World from Servlettttttttt");
+		out.print("Hello World from Servlettttttttt  hai");
 		out.print("Welcome Servlet  :)");
 		out.flush();
 		out.close();
