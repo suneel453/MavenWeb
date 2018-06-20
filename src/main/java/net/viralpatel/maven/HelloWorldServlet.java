c927539857e7f2f3227889d48a3e812267dfe472
suneel453 <suneelkumar453@gmail.com>
2018-06-20 11:59:47 +0530

    Update HelloWorldServlet.java
    comitted [ci skip]

--- src/main/java/net/viralpatel/maven/HelloWorldServlet.java
+++ src/main/java/net/viralpatel/maven/HelloWorldServlet.java
@@ -19,7 +19,7 @@ public class HelloWorldServlet extends HttpServlet {
 		resp.setContentType("text/html");
 		
 		PrintWriter out = resp.getWriter();
-		out.print(" Hey Hello World java by suneel453....!\n");
+		out.print(" Hey Hello World java by suneel....!\n");
 		out.print("Welcome Servlet container ........:)");
 		out.flush();
 		out.close();
